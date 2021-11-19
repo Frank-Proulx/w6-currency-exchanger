4891c7aa8576e1340b5a080ce96ec77e0dfd7704
Frank-Proulx <frankproulx7@gmail.com>
2021-11-19 10:36:26 -0800

    make adjustments to achieve basic functionality of api call

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,11 +1,11 @@
 export class CurrencyService {
-  static getExchangeRate(country) {
+  static getExchangeRate() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
-        return response.json().country;
+        return response.json();
       })
       .catch(function(error) {
         return error;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { CurrencyService } from './currency-service.js';
 
-function displayRate(response, country) {
+function displayRate(response, country, amount) {
   if(response.conversion_rates) {
-    $("outputRate").text(`Your currency amount is ${response.conversion_rates.country}`)
+    $("#outputRate").text(`Your currency amount is ${(response.conversion_rates[country] * amount).toFixed(2)}`);
   } else {
     $("#showError").text(`You have encountered an error: ${response.message}`);
   }
 }
 
-$("#exchange").click(function() {
+$("#exchange").submit(function(event) {
+  event.preventDefault();
   let amount = parseInt($("#amount").val());
   let country = $("#country").val();
   let countryCode = $("#countryCode").val();
+  CurrencyService.getExchangeRate()
+    .then(function(response) {
+      displayRate(response, country, amount);
+    });
 });
