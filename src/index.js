6bfcfd15d7b1d8cb95c6b0ebfbfec888495452a3
Frank-Proulx <frankproulx7@gmail.com>
2021-11-19 11:08:00 -0800

    add link to supported currencies and ability to manually enter country code, with error msg if not correct code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import { CurrencyService } from './currency-service.js';
 
 function displayRate(response, country, amount) {
   if(response.conversion_rates) {
-    $("#outputRate").text(`Your currency amount is ${(response.conversion_rates[country] * amount).toFixed(2)}`);
+    if (response.conversion_rates[country]) {
+      $("#outputRate").text(`Your currency amount is ${(response.conversion_rates[country] * amount).toFixed(2)}`);
+    } else {
+      $("#outputRate").text("Please enter a valid country code from the list of supported currencies");
+    }
   } else {
     $("#showError").text(`You have encountered an error: ${response.message}`);
   }
@@ -16,9 +20,13 @@ $("#exchange").submit(function(event) {
   event.preventDefault();
   let amount = parseInt($("#amount").val());
   let country = $("#country").val();
-  let countryCode = $("#countryCode").val();
+  let countryCode = $("#countryCode").val().toUpperCase();
   CurrencyService.getExchangeRate()
     .then(function(response) {
-      displayRate(response, country, amount);
+      if (country !== "manual") {
+        displayRate(response, country, amount);
+      } else {
+        displayRate(response, countryCode, amount);
+      }
     });
 });
