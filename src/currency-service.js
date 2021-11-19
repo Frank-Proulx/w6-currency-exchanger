0147d9575994fc61e94e95409959d531a1ae3e7b
Frank-Proulx <frankproulx7@gmail.com>
2021-11-19 09:38:14 -0800

    sub out the api key in the fetch for the variable, old key deleted, new key generated

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,6 +1,6 @@
 export class CurrencyService {
   static getExchange() {
-    return fetch(`https://v6.exchangerate-api.com/v6/af8f39a23517174547d86d6b/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(respnose.statusText)
